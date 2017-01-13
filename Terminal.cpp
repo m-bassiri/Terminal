b260cbeb8110f9e664bc5acbd1bc0d99949c2805
m-bassiri <basirimohammad6@gmail.com>
2017-01-13 15:24:31 +0330

    Complete directory management commands
    complete directory management commands

--- Terminal.cpp
+++ Terminal.cpp
@@ -11,10 +11,12 @@
 int main() {
 	char *command=(char*)malloc(500); //string that holds the input command
 	FILE *his = fopen("history.txt", "w+");//file that saves the commands entered by user
-	char *path=(char*)malloc(500);//string that holds absolute path of the pwd
-	path="/users/mohammad";//initializing bsolute path
-	FILE *rootls=fopen("mohammad.ls","a+");//a file that contains list of files in the root directory
+	char path[500]="/users/mohammad/root";//string that holds absolute path of the pwd
+	char curdir[30]="root", curuser[30]="mohammad"; //current directory & user
 	while (1) {
+		color(10);
+		printf("%s @ %s  ",curuser,curdir);
+		color(15);
 		printf(">> ");
 		fgets(command, 500, stdin);//entering the command
 		command = (char*)realloc(command, strlen(command) * sizeof(char) + 5);//returning extra memory to OS
@@ -30,7 +32,7 @@ int main() {
 			struct tm *t;
 			time_t rawtime;
 			time(&rawtime);
-			t = localtime(&rawtime);
+			t = localtime(&rawtime);//saving details of current time in struct
 			printf("The local time of system is %d:%d:%d\n", t->tm_hour, t->tm_min, t->tm_sec);
 			fprintf(his, "   *time\n");
 			continue;
@@ -66,14 +68,49 @@ int main() {
 		}
 		//---------------- make directory command ----------------------
 		else if(!strncmp(command, "mkdir ",6)){
-			command[strlen(command)-1]='\0';
+			command[strlen(command)-1]='\0';//terminating '\n' character
 			struct stat st={0};
-			if(stat(&command[6],&st) == -1){
-				mkdir(&command[6]);
-				fprintf(rootls,strcat(&command[6],"\t"));
+			char *lsname=(char*)malloc(500);
+			char *curlsname=(char*)malloc(500);
+			if(!strcmp(curdir,"root"))
+				strcpy(curlsname,"root.ls");
+			else
+				sprintf(curlsname,"%s/%s.ls",&path[21],curdir);
+			if(command[6]=='/'){ //for absouloute paths
+				if(stat(&command[7],&st) == -1){
+					mkdir(&command[7]);
+					lsname=strcat(&command[7],".ls");
+					FILE *ls=fopen(lsname,"a+");
+					fprintf(ls,"%s\t",&command[7]);
+					fclose(ls);
+				}
+				else
+					printf("The directory already exists.\n");
+			}
+			
+			else if(stat(&command[6],&st) == -1){ //for relative paths
+				if (strcmp(curdir,"root")){
+					char *dname=(char*)malloc(500);
+					sprintf(dname,"%s/%s",&path[21],&command[6]);
+					mkdir(dname);
+					free(dname);
+					sprintf(lsname,"%s/%s/%s.ls",&path[21],&command[6],&command[6]);
+				}
+				else {
+					mkdir(&command[6]);
+					sprintf(lsname,"%s/%s.ls",&command[6],&command[6]);
+				}
+				
+				FILE *ls=fopen(lsname,"a+");
+				FILE *curls=fopen(curlsname,"a+");
+				fprintf(curls,"%s\t",&command[6]);
+				fclose(curls);
+				fclose(ls);
 			}
 			else
 				printf("The directory already exists.\n");
+			free(lsname);
+			free(curlsname);
 			fprintf(his, "   *mkdir %s\n",&command[6]);
 			continue;
 		}
@@ -85,19 +122,71 @@ int main() {
 		}
 		//--------------------- ls command -----------------------------
 		else if(!strcmp(command,"ls\n")){
-			char *lstemp=(char*)malloc(2000);
-			fseek(rootls,0,SEEK_SET);
-			fscanf(rootls,"%s",lstemp);
-			while(!feof(rootls)){
+			char *lsname=(char*)malloc(500);
+			if(!strcmp(curdir,"root"))
+				strcpy(lsname,"root.ls");
+			else
+				sprintf(lsname,"%s/%s.ls",&path[21],curdir);
+			FILE *ls=fopen(lsname,"a+");
+			fseek(ls,0,SEEK_SET);
+			char *lstemp=(char*)malloc(20);
+			fscanf(ls,"%20s",lstemp);
+			while(!feof(ls)){
 				printf("%s\t",lstemp);
-				fscanf(rootls,"%s",lstemp);
+				fscanf(ls,"%20s",lstemp);
 			}
 			puts("");
 			free(lstemp);
+			free(lsname);
+			fclose(ls);
 			fprintf(his, "   *ls\n");
 			continue;
 		}
-		//-----------------
+		//----------------- cd command --------------------------------
+		else if(!strncmp(command,"cd ",3)){
+			command[strlen(command)-1]='\0'; //terminating '\n' character
+			struct stat st={0};
+			if(!strcmp(&command[3],"..")){ // command "cd .."
+				char* temp=(char*)malloc(500);
+				sprintf(temp,path);
+				path[strlen(path)-strlen(curdir)-1]='\0'; //terminating the current dir from pwd
+				sprintf(curdir,predir(temp,curdir));
+				free(temp);
+				fprintf(his, "   *cd ..\n");
+			}
+			
+			else if(command[3]=='/'){ // command "cd abspath"
+				if(stat(&command[4],&st) != -1){
+					strcat(path,&command[3]);
+					char *temp=(char*)malloc(500);
+					sprintf(temp,"%s/t",&command[3]);
+					sprintf(curdir,predir(temp,"t"));
+					free(temp);
+					fprintf(his, "   *cd %s\n",&command[4]);
+				} 
+				else
+					printf("No such diretory!\n");
+			}
+			
+			else { // command "cd dirname"
+				char *dname=(char*)malloc(500);
+				if(!strcmp(curdir,"root"))
+					strcpy(dname,&command[3]);
+				else
+					sprintf(dname,"%s/%s",&path[21],&command[3]); 
+					
+				if(stat(dname,&st) != -1){
+					sprintf(curdir,&command[3]);
+					sprintf(path,"%s/%s",path,curdir); // adding dirname to pwd
+					fprintf(his, "   *cd %s\n",&command[3]);
+				} 
+				else
+					printf("No such diretory!\n");
+				free(dname);
+			}
+			continue;
+		}
+		
 		else printf("Wrong command!\n");
 	}
 	return 0;
