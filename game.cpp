4105301666ef846a05376d5618a96dc38e0154b2
mojtaba-m <mojtaba.05021377@gmail.com>
2017-01-14 20:41:13 +0330

    bazi tacmil shod

--- game.cpp
+++ game.cpp
@@ -2,208 +2,460 @@
 #include <Windows.h>
 #include <time.h>
 
-int adad (int typ11[7][7],int type1[7][7],int x, int y,int n1,int sum);
+int lvl (unsigned int level);
+
+int lvl1 (unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum);
+
+void T (int lvl1(unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum),unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum);
+
+void lvlT1(void T (int lvl1(unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum),unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum),int lvl1(unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum),unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum);
+
+int lvl2 (unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum);
+
+void T2 (int lvl2(unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum),unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum);
+
+void lvlT2(void T2 (int lvl2(unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum),unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum),int lvl2(unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum),unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum);
+
+int lvl3 (unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum);
+
+void T3 (int lvl3(unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum),unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum);
+
+void lvlT3(void T3 (int lvl3(unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum),unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum),int lvl3(unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum),unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum);
 
 int main ()
 {
 	srand(time(NULL));
-	int x, y;
 	int loser = 0;
-	int sum1 = 0;
 	int sum = 0;
-	int n,s,bomb,m1,m2,n1;	
+	char str = 153;
+	char str1 = 223;
+	int m1, m2, x, y;	
+	int pooch = 0;
 	unsigned int wrong = 0;
-	unsigned int type, level;
-	int type1 [7][7]={};
-	int type11[7][7]={};
+	unsigned int level;
+	unsigned int type1 [9][9]={};
+	unsigned int type11[9][9]={};
 
-	for(int j = 0;j < 7;j++){
-		for(int v = 0;v < 7;v++){
-			type11[j][v] = 9;
+
+
+	for(int i = 0;i < 9;i++){
+		for(int j = 0;j < 9;j++){
+			type11[i][j] = 9;
 		}
 	}
-	unsigned int type2 [11][11]={};
-	int type22[11][11]={};
+
+	unsigned int type2 [16][16]={};
+	unsigned int type22[16][16]={};
 	
-	for(int j = 0;j < 11;j++){
-		for(int v = 0;v < 11;v++){
-			type22[j][v] = 9;
+	for(int i = 0;i < 16;i++){
+		for(int j = 0;j < 16;j++){
+			type22[i][j] = 9;
 		}
 	}
-	unsigned int type3 [15][15]={};
-	int type33[15][15]={};
+	unsigned int type3 [22][22]={};
+	unsigned int type33[22][22]={};
 	
-	for(int j = 0;j < 15;j++){
-		for(int v = 0;v < 15;v++){
-			type33[j][v] = 9;
+	for(int i = 0;i < 22;i++){
+		for(int j = 0;j < 22;j++){
+			type33[i][j] = 9;
 		}
-	}
+	}//tarif motaghayyer ha
 
-	printf("%s\n%s\n%s\n%s\n%s","please choose type of game: "
-							,"type 1: 7 * 7"
-							,"type 2: 11 * 11"
-							,"type 3: 15 * 15"
-							,"type : ");
-	scanf("%u",&type);
-
-	if(type == 1){
-		n = 7 * 7;
-		n1 = 7;
-	}
-	else{
-		if(type == 2){
-			n = 11 * 11;
-			n1 = 11;
-		}
-		else{
-			if(type == 3){
-				n = 15 * 15;
-				n1 = 15;
-			}
-			else{	
-				while(wrong != 1){
-					printf("%s\n%s","you intered wrong number!!!"
-									,"please choose again: ");
-					scanf("%u",&type);
-					if(type == 1 || type == 2 || type == 3)
-						wrong = 1;
-				}
-			}
-		}
-	}
-	wrong = 0;
-	printf("%s\n%s\n%s\n%s\n%s","please choose level of game:"
-							,"level esey   = 1"
-						    ,"level normal = 2"
-						    ,"level hard   = 3"
+	printf("%s\n%s\n%s\n%s\n%s","please choose level of game: "
+							,"level 1: 9 * 9"
+							,"level 2: 16 * 16"
+							,"level 3: 22 * 22"
 							,"level : ");
 	scanf("%u",&level);
-	if(level == 1)
-		s = 6;
-	else{
-		if(level == 2)
-			s = 5;
-		else{
-			if(level == 3)
-				s = 4;
-			else{	
-				while(wrong != 1){
-					printf("%s\n%s","you intered wrong number!!!"
-									,"please choose again: ");
-					scanf("%u",&level);
-					if(level == 1 || level == 2 || level == 3)
-						wrong = 1;
-				}
-			}
-		}
-	}//end first part
 
-	bomb = n / s;
-	if(type == 1){
-		for (int i = 0; i < bomb;i++){
-			m1 = rand() % n1;
-			m2 = rand() % n1;
-			if(type1 [m1][m2] == 0)
-				type1 [m1][m2] = 1;
-			else{
+	level = lvl(level);
+	
+	switch(level){
+	case 1:
+		for (int i = 0; i < 10;i++){
+			m1 = rand() % 9;
+			m2 = rand() % 9;
+			if(type1 [m1][m2] != 0){
 				i--;
 			}
-		}
-	}
-	else{
-		if(type == 2){
-			for (int i = 0; i < bomb;i++){
-				m1 = rand() % n1;
-				m2 = rand() % n1;
-				if(type2 [m1][m2] == 0)
-					type2 [m1][m2] = 1;
-				else{
-					i--;
-				}
+			else{
+				type1 [m1][m2] = 1;
 			}
 		}
-		else{
-			for (int i = 0; i < bomb;i++){
-				m1 = rand() % n1;
-				m2 = rand() % n1;
-				if(type3 [m1][m2] == 0)
-					type3 [m1][m2] = 1;
-				else{
-					i--;
-				}
+		break;
+	case 2:
+		for (int i = 0; i < 40;i++){
+			m1 = rand() % 16;
+			m2 = rand() % 16;
+			if(type2 [m1][m2] != 0){
+				i--;
+			}
+			else{
+				type2 [m1][m2] = 1;
 			}
 		}
-	}
-	for(int j = 0;j < n1;j++){
-		for(int v = 0;v < n1;v++){
-			printf("%u ",type1[j][v]);
+	case 3:
+		for (int i = 0; i < 90;i++){
+			m1 = rand() % 22;
+			m2 = rand() % 22;
+			if(type3 [m1][m2] != 0){
+				i--;
+			}
+			else{
+				type3 [m1][m2] = 1;
+			}
 		}
-		printf("\n");
 	}
 	//start game with x & y
-	while(!feof(stdin)){
-		printf("%s\n%s ","please inter moghtasat","inter x: ");
-		scanf("%d",&x);
-		printf("inter y: ");
-		scanf("%d",&y);
-		x--;
-		y--;
-		if(type1[x][y] == 1)//age too ghone bomb bood
-		{
-			loser = 1;
-			break;
-		}
-
-		else//age to ghone bomb nabod
-		{
-			adad (type11,type1,x,y,n1,sum);
-		}
-		for(int i = 0;i < n1;i++){
-			for(int j = 0;j < n1;j++){
-				if(type11[i][j] == 9){
-					sum1++;
+	switch (level){
+	case 1:
+		while(!feof(stdin)){
+			printf("%s\n%s","please inter moghtasat","inter x: ");
+			scanf("%d",&x);
+			printf("inter y: ");
+			scanf("%d",&y);
+			x--;
+			y--;
+			if(type1[x][y] == 0){
+				lvlT1(T,lvl1,type11,type1,x,y,sum);			
+				for(int o = 0; o < 81; o++){
+					for(int i = 0;i < 9;i++){
+						for(int j = 0;j < 9;j++){
+							if(type11[i][j] == 0){
+								lvlT1(T,lvl1,type11,type1,i,j,sum);
+							}
+						}
+					}
+				}
+				for(int j = 0;j < 9;j++){
+					for(int i = 0;i < 9;i++){
+						if (type11[j][i] == 0)
+							printf("%c ",str);
+						else{
+							if(type11[j][i] == 9)
+								printf("%c ",str1);
+							else
+							{
+								printf("%u ",type11[j][i]);
+							}
+						}
+					}
+					printf("\n");
+					printf("\n");
 				}
 			}
-		}
-		if(sum1 == bomb)
-			break;
-		for(int j = 0;j < n1;j++){
-			for(int v = 0;v < n1;v++){
-				printf("%u ",type11[j][v]);
+			else{
+				loser = 1;
+				break;
 			}
-			printf("\n");
+			pooch = 0;
+			for(int j = 0;j < 9;j++){
+				for(int i = 0;i < 9;i++){
+					if(type11[j][i] != 9)
+						pooch++;
+				}
+			}
+			if(pooch == 71)
+				break;
 		}
-	}
-	if(loser == 1)
-		printf("you lose\n");
-	else
-		printf("you win\n");
-		
-		
-}//تابعی که مخنصات یه خونه که بمب توش نیست رو میگیره و اگه عدد بود چاپ میکنه و اگه پوچ بود اونو با 0 نشون میده و میره سراغ خونه های اطرافش
+		break;
+	case 2:
+		while(!feof(stdin)){
+			printf("%s\n%s","please inter moghtasat","inter x: ");
+			scanf("%d",&x);
+			printf("inter y: ");
+			scanf("%d",&y);
+			x--;
+			y--;
+			if(type1[x][y] == 0)
+			{
+				lvlT2(T2,lvl2,type22,type2,x,y,sum);
+			
+				for(int o = 0; o < 81; o++){
+					for(int i = 0;i < 16;i++){
+						for(int j = 0;j < 16;j++){
+							if(type22[i][j] == 0){
+								lvlT2(T2,lvl2,type22,type2,i,j,sum);
+							}
+						}
+					}
+				}
+				for(int j = 0;j < 16;j++){
+					for(int i = 0;i < 16;i++){
+						if (type22[j][i] == 0)
+							printf("%c ",str);
+						else{
+							if(type22[j][i] == 9)
+								printf("%c ",str1);
+							else
+							{
+								printf("%u ",type22[j][i]);
+							}
+						}
+					}
+					printf("\n");
+					printf("\n");
+				}
+			}
+			else{
+				loser = 1;
+				break;
+			}
+			pooch = 0;
+			for(int j = 0;j < 16;j++){
+				for(int i = 0;i < 16;i++){
+					if(type11[j][i] != 9)
+						pooch++;
+				}
+			}
+			if(pooch == 216)
+				break;
+		}
+		break;
+	case 3:
 
-int adad (int type11[7],int type1[7][7],int x, int y,int n1,int sum)//این تابع رو برای جدول 7*7 درست کردم فعلا
+
+		while(!feof(stdin)){
+			printf("%s\n%s","please inter moghtasat","inter x: ");
+			scanf("%d",&x);
+			printf("inter y: ");
+			scanf("%d",&y);
+			x--;
+			y--;
+			if(type3[x][y] == 0)
+			{
+				lvlT3(T3,lvl3,type33,type3,x,y,sum);
+
+				for(int o = 0; o < 81; o++){
+					for(int i = 0;i < 22;i++){
+						for(int j = 0;j < 22;j++){
+							if(type33[i][j] == 0){
+								lvlT3(T3,lvl3,type33,type3,i,j,sum);
+							}
+						}
+					}
+				}
+				for(int j = 0;j < 22;j++){
+					for(int i = 0;i < 22;i++){
+						if (type33
+							[j][i] == 0)
+							printf("%c ",str);
+						else{
+							if(type33[j][i] == 9)
+								printf("%c ",str1);
+							else
+							{
+								printf("%u ",type33[j][i]);
+							}
+						}
+					}
+					printf("\n");
+					printf("\n");
+				}
+			}
+			else
+			{
+				loser = 1;
+				break;
+			}
+					pooch = 0;
+			for(int j = 0;j < 22;j++){
+				for(int i = 0;i < 22;i++){
+					if(type33[j][i] != 9)
+						pooch++;
+				}
+			}
+			if(pooch == 394)
+				break;
+			
+			}
+		break;
+		}
+	if(loser == 0)
+		printf("you win\n");
+	else
+		printf("you lose\n");
+	system("pause");
+}
+
+int lvl (unsigned int level)
+{
+	
+	if(level != 1 && level != 2 && level != 3){
+		printf("%s\n%s","you intered wrong number!!!"
+					   ,"please choose again: ");
+		scanf("%u",&level);
+		return lvl (level);
+	}
+	else{
+		return level;
+	}
+}
+
+int lvl1 (unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum)
 {
 	sum = 0;
 	if(type1[x][y] == 0){
-		for(int i = -1; i < 2; i++){
-			for(int j = -1; j < 2; j++){
-				if(x+i < 7 && y+j < 7 && x+i > 0 && y+j > 0){
-					sum = sum + type1[x+i][y+j];
-				}
-			}
-		}
-		if(sum == 0){
-			type11[x][y] = 0;
+		for(int j = -1; j < 2; j++){
 			for(int i = -1; i < 2; i++){
-				for(int j = -1; j < 2; j++){
-					if(x+i < 7 && y+j < 7 && x+i > 0 && y+j > 0)
-					return adad (type11,type1,x+i,y+j,n1,sum);
+				if(x+i < 9 && y+j < 9 && x+i > -1 && y+j > -1){
+					if(x+i != x || y+j != y){
+					sum = sum + type1[x+i][y+j];
+					}
 				}
-			}
+			}		
 		}
-		else{
-			type11[x][y] = sum;
+		type11[x][y] = sum;
+		if(type11[x][y] == 0){
+			return 0;
+		}
+		else
+		{
+			return 1;
 		}
 	}
 }
+
+void T (int lvl1(unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum),unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum){
+	if(lvl1 (type11,type1,x,y,sum) == 0){
+		for(int k = -1; k < 2; k++){
+			for(int l = -1; l < 2; l++){
+				if(x+l < 9 && y+k < 9 && x+l > -1 && y+k > -1){
+					if(x+l != x || y+k != y){
+					lvl1(type11,type1,x+l,y+k,sum);
+					}
+				}
+			}
+		}
+	}
+}
+
+void lvlT1(void T (int lvl1(unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum),unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum),int lvl1(unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum),unsigned int type11[9][9],unsigned int type1[9][9],int x, int y,int sum){
+	if(lvl1 (type11,type1,x,y,sum) == 0){
+		for(int k = -1; k < 2; k++){
+			for(int l = -1; l < 2; l++){
+				if(x+l < 9 && y+k < 9 && x+l > -1 && y+k > -1){
+					if(x+l != x || y+k != y){
+						if(lvl1(type11,type1,x+l,y+k,sum)==0){
+							T (lvl1,type11,type1,x+l,y+k,sum);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+int lvl2 (unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum)
+{
+	sum = 0;
+	if(type2[x][y] == 0){
+		for(int j = -1; j < 2; j++){
+			for(int i = -1; i < 2; i++){
+				if(x+i < 16 && y+j < 16 && x+i > -1 && y+j > -1){
+					if(x+i != x || y+j != y){
+					sum = sum + type2[x+i][y+j];
+					}
+				}
+			}		
+		}
+		type22[x][y] = sum;
+		if(type22[x][y] == 0){
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+}
+
+void T2 (int lvl2(unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum),unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum)
+{
+	if(lvl2 (type22,type2,x,y,sum) == 0){
+		for(int k = -1; k < 2; k++){
+			for(int l = -1; l < 2; l++){
+				if(x+l < 9 && y+k < 9 && x+l > -1 && y+k > -1){
+					if(x+l != x || y+k != y){
+					lvl2(type22,type2,x+l,y+k,sum);
+					}
+				}
+			}
+		}
+	}
+}
+
+void lvlT2(void T2 (int lvl2(unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum),unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum),int lvl2(unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum),unsigned int type22[16][16],unsigned int type2[16][16],int x, int y,int sum)
+{
+	if(lvl2 (type22,type2,x,y,sum) == 0){
+		for(int k = -1; k < 2; k++){
+			for(int l = -1; l < 2; l++){
+				if(x+l < 16 && y+k < 16 && x+l > -1 && y+k > -1){
+					if(x+l != x || y+k != y){
+						if(lvl2(type22,type2,x+l,y+k,sum)==0){
+							T2 (lvl2,type22,type2,x+l,y+k,sum);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+int lvl3 (unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum)
+{
+	sum = 0;
+	if(type3[x][y] == 0){
+		for(int j = -1; j < 2; j++){
+			for(int i = -1; i < 2; i++){
+				if(x+i < 22 && y+j < 22 && x+i > -1 && y+j > -1){
+					if(x+i != x || y+j != y){
+					sum = sum + type3[x+i][y+j];
+					}
+				}
+			}		
+		}
+		type33[x][y] = sum;
+		if(type33[x][y] == 0){
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+}
+
+void T3 (int lvl3(unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum),unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum)
+{
+	if(lvl3 (type33,type3,x,y,sum) == 0){
+		for(int k = -1; k < 2; k++){
+			for(int l = -1; l < 2; l++){
+				if(x+l < 22 && y+k < 22 && x+l > -1 && y+k > -1){
+					if(x+l != x || y+k != y){
+						lvl3(type33,type3,x+l,y+k,sum);
+					}
+				}
+			}
+		}
+	}
+}
+
+void lvlT3(void T3 (int lvl3(unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum),unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum),int lvl3(unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum),unsigned int type33[22][22],unsigned int type3[22][22],int x, int y,int sum)
+{
+	if(lvl3 (type33,type3,x,y,sum) == 0){
+		for(int k = -1; k < 2; k++){
+			for(int l = -1; l < 2; l++){
+				if(x+l < 22 && y+k < 22 && x+l > -1 && y+k > -1){
+					if(x+l != x || y+k != y){
+						if(lvl3(type33,type3,x+l,y+k,sum)==0){
+							T3 (lvl3,type33,type3,x+l,y+k,sum);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+
